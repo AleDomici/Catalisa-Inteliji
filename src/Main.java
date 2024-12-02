bd5c9e30517877af98e5d313b216478d3b82d342
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-02 14:32:57 -0300

    Primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         int number = 10;
         String phrase = "Hello, World!";
         System.out.println(number);
